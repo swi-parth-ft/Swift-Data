56bec77d8960aeb2bf87f8e766249babc38ac6d4
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-13 11:23:47 -0400

    Relationships with SwiftData, SwiftUI, and @Query

--- Swift-Data/ContentView.swift
+++ Swift-Data/ContentView.swift
@@ -13,11 +13,25 @@ class User {
     var name: String
     var city: String
     var joinDate: Date
-
+    @Relationship(deleteRule: .cascade) var jobs = [Job]()
+    
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
         self.joinDate = joinDate
+    }
+}
+
+@Model
+class Job {
+    var title: String
+    var priority: Int
+    var owner: User?
+    
+    init(title: String, priority: Int, owner: User? = nil) {
+        self.title = title
+        self.priority = priority
+        self.owner = owner
     }
 }
 
@@ -35,7 +49,7 @@ struct ContentView: View {
         }
         
     }
-        ,sort: \User.name) var users: [User]
+           ,sort: \User.name) var users: [User]
     
     @State private var showUpcomingOnly = false
     @State private var sortOrder = [
@@ -46,41 +60,49 @@ struct ContentView: View {
         NavigationStack() {
             UserView(minimumJoinDate: showUpcomingOnly ? .now : .distantPast, sortDescriptor: sortOrder)
                 .navigationTitle("Users")
-            .toolbar {
-                Button("Add Users", systemImage: "plus") {
-                    try? modelContext.delete(model: User.self)
-                    
-                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-                                let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
-                                let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-                                let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-
-                                modelContext.insert(first)
-                                modelContext.insert(second)
-                                modelContext.insert(third)
-                                modelContext.insert(fourth)
-                }
-                
-                Button(showUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
-                    showUpcomingOnly.toggle()
-                }
-                
-                Menu("Sort", systemImage: "arrow.up.arrow.down") {
-                    Picker("Sort By", selection: $sortOrder) {
-                        Text("Sort by name")
-                            .tag([
-                                SortDescriptor(\User.name),
-                                SortDescriptor(\User.joinDate),
-                            ])
+                .toolbar {
+                    Button("Add Users", systemImage: "plus") {
+                        try? modelContext.delete(model: User.self)
                         
-                        Text("Sort by join date")
-                            .tag([
-                                SortDescriptor(\User.joinDate),
-                                SortDescriptor(\User.name),
-                            ])
+                        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                        let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                        let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                        let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                        
+                        let job1 = Job(title: "Organize sock drawer", priority: 3)
+                        let job2 = Job(title: "Make plans with Alex", priority: 4)
+                        let job3 = Job(title: "Clean up kitchen", priority: 2)
+                        
+                        first.jobs.append(job1)
+                        first.jobs.append(job2)
+                        second.jobs.append(job3)
+                        
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(fourth)
+                    }
+                    
+                    Button(showUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                        showUpcomingOnly.toggle()
+                    }
+                    
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort By", selection: $sortOrder) {
+                            Text("Sort by name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate),
+                                ])
+                            
+                            Text("Sort by join date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name),
+                                ])
+                        }
                     }
                 }
-            }
         }
     }
 }
@@ -90,8 +112,20 @@ struct UserView: View {
     @Query var users: [User]
     var body: some View {
         List(users) { user in
-                Text(user.name)
-            }
+            HStack {
+                   Text(user.name)
+
+                   Spacer()
+
+                   Text(String(user.jobs.count))
+                       .fontWeight(.black)
+                       .padding(.horizontal, 10)
+                       .padding(.vertical, 5)
+                       .background(.blue)
+                       .foregroundStyle(.white)
+                       .clipShape(.capsule)
+               }
+        }
     }
     
     init(minimumJoinDate: Date, sortDescriptor: [SortDescriptor<User>]) {
